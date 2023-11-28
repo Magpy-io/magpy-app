9584779aa3140ae9ad28764e37aa1d89e1d588fb
Issam <issambenelgada@gmail.com>
2023-11-28 19:56:39 +0100

    Switched back to server as main page.

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -18,7 +18,7 @@ const Navigation = () => {
         screenOptions={{
           headerShown: false,
         }}
-        initialRouteName="Test"
+        initialRouteName="Server"
       >
         <Drawer.Screen name="Server" component={PhotoGalleryServerScreen} />
         <Drawer.Screen name="Local" component={PhotoGalleryLocalScreen} />
