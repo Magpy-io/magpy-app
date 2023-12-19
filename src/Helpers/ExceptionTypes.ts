c7ae842b97a4e7882edae682a162c90fa68ea8d7
Issam <issambenelgada@gmail.com>
2023-12-19 23:51:48 +0100

    Added the rest of queries.

--- src/Helpers/ExceptionTypes.ts
+++ src/Helpers/ExceptionTypes.ts
@@ -1,1 +1,13 @@
-export class ErrorBackendUnreachable extends Error {}
+export class ErrorBackendUnreachable extends Error {
+    constructor() {
+        super();
+        this.message = 'Backend server is unreachable';
+    }
+}
+
+export class ErrorNoUserToken extends Error {
+    constructor() {
+        super();
+        this.message = 'You need to log in first to get a user token';
+    }
+}
--- src/Helpers/QueriesBackend.tsx
+++ src/Helpers/QueriesBackend.tsx
@@ -1,9 +1,27 @@
-import axios from 'axios';
+import axios, {AxiosResponse} from 'axios';
 
-import {ErrorBackendUnreachable} from '~/Helpers/ExceptionTypes';
+import {ErrorBackendUnreachable, ErrorNoUserToken} from '~/Helpers/ExceptionTypes';
+
+export {ErrorBackendUnreachable, ErrorNoUserToken};
 
 const host = '192.168.0.15';
 const port = '8001';
+
+let UserToken = '';
+
+export function HasUserToken(): boolean {
+    if (UserToken) {
+        return true;
+    }
+    return false;
+}
+
+export function GetUserToken(): string {
+    if (!UserToken) {
+        throw new ErrorNoUserToken();
+    }
+    return UserToken;
+}
 
 const path = () => {
     return `http://${host}:${port}/`;
@@ -12,6 +30,15 @@ const path = () => {
 const routes = {
     registerUser: () => {
         return path() + 'register/';
+    },
+    loginUser: () => {
+        return path() + 'login/';
+    },
+    whoAmI: () => {
+        return path() + 'whoami/';
+    },
+    getMyServerInfo: () => {
+        return path() + 'getMyServerInfo/';
     },
 };
 
@@ -37,34 +64,41 @@ type ErrorsAuthorization =
     | ErrorAuthorizationWrongFormat;
 
 export type ServerResponseMessage = {
-    ok: boolean;
+    ok: true;
     message: string;
 };
 
+export type ServerResponseData<T> = {
+    ok: true;
+    data: T;
+};
+
 export type ServerResponseError<Errors> = {
-    ok: boolean;
+    ok: false;
     message: string;
     errorCode: Errors | ErrorBadRequest | ErrorServerError;
 };
 
-export type ServerResponseData<T> = {
-    ok: boolean;
-    data: T;
-};
+export type EndpointMethodsResponseType<T, U> = Promise<T | ServerResponseError<U>>;
 
-export type RegisterRequestDataType = {
+// Register
+export type RegisterRequestData = {
     email: string;
     name: string;
     password: string;
 };
 
-export type RegisterResponseError = ServerResponseError<
-    ErrorEmailTaken | ErrorInvalidEmail | ErrorInvalidName | ErrorInvalidPassword
->;
+export type RegisterResponseData = ServerResponseMessage;
+
+export type RegisterResponseErrorTypes =
+    | ErrorEmailTaken
+    | ErrorInvalidEmail
+    | ErrorInvalidName
+    | ErrorInvalidPassword;
 
 export async function register(
-    data: RegisterRequestDataType
-): Promise<ServerResponseMessage | RegisterResponseError> {
+    data: RegisterRequestData
+): EndpointMethodsResponseType<RegisterResponseData, RegisterResponseErrorTypes> {
     try {
         const response = await axios.post(routes.registerUser(), data);
         return response.data;
@@ -73,12 +107,102 @@ export async function register(
     }
 }
 
+// Login
+export type LoginRequestData = {
+    email: string;
+    password: string;
+};
+
+export type LoginResponseData = ServerResponseMessage;
+
+export type LoginResponseErrorTypes = ErrorInvalidCredentials;
+
+export async function login(
+    data: LoginRequestData
+): EndpointMethodsResponseType<LoginResponseData, LoginResponseErrorTypes> {
+    try {
+        const response = await axios.post(routes.loginUser(), data);
+        UserToken = extractToken(response);
+        return response.data;
+    } catch (err: any) {
+        return handleAxiosError(err);
+    }
+}
+
+// WhoAmI
+export type WhoAmIRequestData = {};
+
+export type WhoAmIResponseData = ServerResponseData<{
+    user: {_id: string; email: string};
+}>;
+
+export type WhoAmIResponseErrorTypes = ErrorsAuthorization;
+
+export async function whoAmI(
+    data: WhoAmIRequestData
+): EndpointMethodsResponseType<WhoAmIResponseData, WhoAmIResponseErrorTypes> {
+    verifyHasToken();
+    try {
+        const response = await axios.post(routes.whoAmI(), data, authorizationObject());
+        return response.data;
+    } catch (err: any) {
+        return handleAxiosError(err);
+    }
+}
+
+// GetMyServerInfo
+export type GetMyServerInfoRequestData = {};
+
+export type GetMyServerInfoResponseData = ServerResponseData<{
+    server: {_id: string; name: string; ip: string};
+}>;
+
+export type GetMyServerInfoResponseErrorTypes = ErrorNoAssociatedServer | ErrorsAuthorization;
+
+export async function getMyServerInfo(
+    data: GetMyServerInfoRequestData
+): EndpointMethodsResponseType<
+    GetMyServerInfoResponseData,
+    GetMyServerInfoResponseErrorTypes
+> {
+    verifyHasToken();
+    try {
+        const response = await axios.post(
+            routes.getMyServerInfo(),
+            data,
+            authorizationObject()
+        );
+        return response.data;
+    } catch (err: any) {
+        return handleAxiosError(err);
+    }
+}
+
+// Functions
 function handleAxiosError(err: any): ServerResponseError<any> {
     if (err.response) {
         return err.response.data;
     } else if (err.request) {
-        throw new ErrorBackendUnreachable('Backend unreachable');
+        throw new ErrorBackendUnreachable();
     } else {
         throw err;
+    }
+}
+
+function extractToken(response: AxiosResponse<any, any>) {
+    return response.headers['authorization'].toString().split(' ')[1];
+}
+
+function authorizationObject() {
+    return {
+        headers: {
+            Authorization: `Bearer ${UserToken}`,
+        },
+    };
+}
+
+function verifyHasToken() {
+    if (!UserToken) {
+        throw new ErrorNoUserToken();
     }
 }
