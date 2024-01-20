5f62fdc3993b411bf547a57ad9a95dadde606c5f
Issam <issambenelgada@gmail.com>
2024-01-20 12:44:11 +0100

    Changed eslint and prettier configs.

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+  ],
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'all',
+  printWidth: 95,
+  tabWidth: 2,
+  singleQuote: true,
+  bracketSpacing: true,
+  importOrder: ['^react$|^react-native$', '^[A-Za-z].*|^@.*', '^[~].*', '^[./]'],
+  importOrderSeparation: true,
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+};
