170debb734142df87be21bd0091c3f0212c6cb15
Chaimaa <chaimaa.elassad@gmail.com>
2024-02-10 21:23:35 +0100

    fix title color

--- src/Navigation/Screens/SettingsStackNavigation/AccountSettingsScreen.tsx
+++ src/Navigation/Screens/SettingsStackNavigation/AccountSettingsScreen.tsx
@@ -5,12 +5,13 @@ import { Text } from 'react-native-elements';
 
 import { useStyles } from '~/Hooks/useStyles';
 import { colorsType } from '~/Styles/colors';
+import { typography } from '~/Styles/typography';
 
 export default function AccountSettingsScreen() {
   const styles = useStyles(makeStyles);
   return (
     <View style={styles.container}>
-      <Text>Account settings</Text>
+      <Text style={styles.text}>Account settings</Text>
     </View>
   );
 }
@@ -22,5 +23,8 @@ const makeStyles = (colors: colorsType) =>
       paddingHorizontal: 20,
       paddingVertical: 60,
       backgroundColor: colors.BACKGROUND,
+    },
+    text: {
+      ...typography(colors).mediumText,
     },
   });
