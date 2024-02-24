542bfad13db575e1ff5c396d9bacf0a7cfdaff77
Chaimaa <chaimaa.elassad@gmail.com>
2024-02-24 13:15:30 +0100

    Add minHeight and maxHeight to bottomModal

--- src/Components/CommonComponents/BottomModal.tsx
+++ src/Components/CommonComponents/BottomModal.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
+import { useOrientation } from '~/Hooks/useOrientation';
+import { spacing } from '~/Styles/spacing';
+
 import GenericModal from './GenericModal/GenericModal';
+
+const DEFAULT_MIN_HEIGHT = 0;
+const DEFAULT_MAX_HEIGHT = 0.9;
 
 type BottomModalProps = {
   modalVisible: boolean;
   onRequestClose: () => void;
   children: JSX.Element;
+  maxHeight?: number;
+  minHeight?: number;
 };
 
 export default function BottomModal({
   modalVisible,
   onRequestClose,
   children,
+  maxHeight = DEFAULT_MAX_HEIGHT,
+  minHeight = DEFAULT_MIN_HEIGHT,
 }: BottomModalProps) {
+  const { height } = useOrientation();
   return (
     <GenericModal
       modalVisible={modalVisible}
       onRequestClose={onRequestClose}
       animationType="slide"
-      style={styles.viewStyle}>
+      style={[
+        styles.viewStyle,
+        { maxHeight: height * maxHeight, minHeight: height * minHeight },
+      ]}>
       {children}
     </GenericModal>
   );
@@ -32,7 +46,6 @@ const styles = StyleSheet.create({
     bottom: 0,
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-    padding: 20,
-    paddingTop: 30,
+    padding: spacing.spacing_xl,
   },
 });
