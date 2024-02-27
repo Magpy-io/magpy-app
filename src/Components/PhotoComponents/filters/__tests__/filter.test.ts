083674159cf2562f9d704087677f9e99f49d42bd
Chaimaa <chaimaa.elassad@gmail.com>
2024-02-27 18:52:54 +0100

    change useless test names to issam approved ones

--- src/Components/PhotoComponents/filters/__tests__/filter.test.ts
+++ src/Components/PhotoComponents/filters/__tests__/filter.test.ts
@@ -3,7 +3,7 @@ import { makeGalleryPhoto } from '~/Context/ReduxStore/Slices/Photos/__tests__/M
 import { DateFilter } from '../DateFilter';
 import { StatusFilter } from '../StatusFilter';
 
-describe('Tests for function filterPhotos : StatusFilter', () => {
+describe('Tests for StatusFilter class', () => {
   const inServerFilter = new StatusFilter({ value: 'inServer' });
   const inDeviceFilter = new StatusFilter({ value: 'inDevice' });
 
@@ -11,26 +11,26 @@ describe('Tests for function filterPhotos : StatusFilter', () => {
   const serverPhoto = makeGalleryPhoto({ serverId: 'ServerId' });
   serverPhoto.mediaId = undefined;
 
-  it('Should return photos in server if status filter value is inServer ', () => {
+  it('Should return only the photo in server when passed two photos : one in server, one in device', () => {
     const photos = [devicePhoto, serverPhoto];
     const filteredPhotos = inServerFilter.filter(photos);
     expect(filteredPhotos).toStrictEqual([serverPhoto]);
   });
 
-  it('Should return photos in device if status filter value is inDevice ', () => {
+  it('Should return only the photo in device when passed two photos : one in server, one in device', () => {
     const photos = [devicePhoto, serverPhoto];
     const filteredPhotos = inDeviceFilter.filter(photos);
     expect(filteredPhotos).toStrictEqual([devicePhoto]);
   });
 
-  it('Should return [] if status filter value is inDevice and no photos are in device ', () => {
+  it('Should return [] if passed only one server photo', () => {
     const photos = [serverPhoto];
     const filteredPhotos = inDeviceFilter.filter(photos);
     expect(filteredPhotos).toStrictEqual([]);
   });
 });
 
-describe('Tests for function filterPhotos : DateFilter', () => {
+describe('Tests DateFilter class', () => {
   const dateFilter = new DateFilter({
     fromDate: '2024-01-01T12:30:00.000Z',
     toDate: '2024-01-05T12:30:00.000Z',
@@ -41,13 +41,13 @@ describe('Tests for function filterPhotos : DateFilter', () => {
   const photoInRange = makeGalleryPhoto({ date: dateInRange });
   const photoNotInRange = makeGalleryPhoto({ date: dateNotInRange });
 
-  it('Should return the photo in date range', () => {
+  it('Should return the photo in date range when passed two photos : one in date range, one not in date range', () => {
     const photos = [photoInRange, photoNotInRange];
     const filteredPhotos = dateFilter.filter(photos);
     expect(filteredPhotos).toStrictEqual([photoInRange]);
   });
 
-  it('Should return [] when no photos are in date range', () => {
+  it('Should return [] when passed one photo not in date range', () => {
     const photos = [photoNotInRange];
     const filteredPhotos = dateFilter.filter(photos);
     expect(filteredPhotos).toStrictEqual([]);
