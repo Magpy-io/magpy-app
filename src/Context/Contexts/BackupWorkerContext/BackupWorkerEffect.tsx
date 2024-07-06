4c2df29110e0b4801b5d5d4b84d9e3abecab5ca3
Issam Benelgada <issambenelgada@gmail.com>
2024-07-06 19:50:22 +0200

    Added effect to update android worker network data when changed in app.

--- src/Context/Contexts/BackupWorkerContext/BackupWorkerEffect.tsx
+++ src/Context/Contexts/BackupWorkerContext/BackupWorkerEffect.tsx
@@ -2,14 +2,31 @@ import React, { ReactNode, useEffect } from 'react';
 
 import { AutoBackupModule } from '~/NativeModules/AutoBackupModule';
 
+import { useServerContext } from '../ServerContext';
 import { useBackupWorkerContextSettersInner } from './BackupWorkerContext';
+import {
+  useBackupWorkerContext,
+  useBackupWorkerContextFunctions,
+} from './useBackupWorkerContext';
 
 type PropsType = {
   children: ReactNode;
 };
 
 export const BackupWorkerEffect: React.FC<PropsType> = props => {
+  const { autobackupEnabled } = useBackupWorkerContext();
   const { setAutobackupEnabled } = useBackupWorkerContextSettersInner();
+  const { StartAutoBackup } = useBackupWorkerContextFunctions();
+
+  const { isServerReachable, serverPath, token } = useServerContext();
+
+  // This effect will update the server network data of worker
+  // whenever the server network data changes
+  useEffect(() => {
+    if (isServerReachable && autobackupEnabled) {
+      StartAutoBackup().catch(console.log);
+    }
+  }, [StartAutoBackup, autobackupEnabled, isServerReachable, serverPath, token]);
 
   useEffect(() => {
     AutoBackupModule.IsWorkerAlive()
