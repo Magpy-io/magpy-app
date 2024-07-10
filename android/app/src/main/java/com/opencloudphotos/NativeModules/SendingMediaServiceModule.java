204c476b11522489dc0f7cdb195f9675c0227469
Issam Benelgada <issambenelgada@gmail.com>
2024-07-10 13:09:43 +0200

    Deleted some unused imports.

--- android/app/src/main/java/com/opencloudphotos/NativeModules/SendingMediaServiceModule.java
+++ android/app/src/main/java/com/opencloudphotos/NativeModules/SendingMediaServiceModule.java
@@ -3,8 +3,6 @@ package com.opencloudphotos.NativeModules;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
-import android.media.ExifInterface;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,10 +20,6 @@ import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableArray;
 import com.opencloudphotos.Services.SendingMediaForegroundService;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class SendingMediaServiceModule extends ReactContextBaseJavaModule{
     public SendingMediaServiceModule(ReactApplicationContext context) {
