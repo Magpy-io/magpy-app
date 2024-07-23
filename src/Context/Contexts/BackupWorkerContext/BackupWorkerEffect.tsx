5e1e56bc4b54127d6c12fdcde4733a0ee4602242
Issam Benelgada <issambenelgada@gmail.com>
2024-07-23 14:06:23 +0200

    Fixed bug where worker would get started twice.

--- src/Context/Contexts/BackupWorkerContext/BackupWorkerEffect.tsx
+++ src/Context/Contexts/BackupWorkerContext/BackupWorkerEffect.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect } from 'react';
+import React, { ReactNode, useEffect, useMemo, useRef } from 'react';
 
 import { AutoBackupModule } from '~/NativeModules/AutoBackupModule';
 
@@ -20,13 +20,27 @@ export const BackupWorkerEffect: React.FC<PropsType> = props => {
 
   const { isServerReachable, serverPath, token } = useServerContext();
 
+  const serverInfo = useMemo(() => {
+    return { serverPath, token };
+  }, [serverPath, token]);
+
+  const lastServerInfoRef = useRef(serverInfo);
+  const lastIsServerREachable = useRef(isServerReachable);
+
   // This effect will update the server network data of worker
-  // whenever the server network data changes
+  // whenever the server network data changes or the reachable status goes from false to true
   useEffect(() => {
-    if (isServerReachable && autobackupEnabled) {
+    if (
+      isServerReachable &&
+      autobackupEnabled &&
+      (lastServerInfoRef.current !== serverInfo ||
+        lastIsServerREachable.current !== isServerReachable)
+    ) {
       StartAutoBackup().catch(console.log);
     }
-  }, [StartAutoBackup, autobackupEnabled, isServerReachable, serverPath, token]);
+    lastServerInfoRef.current = serverInfo;
+    lastIsServerREachable.current = isServerReachable;
+  }, [StartAutoBackup, autobackupEnabled, isServerReachable, serverInfo]);
 
   useEffect(() => {
     AutoBackupModule.IsWorkerAlive()
