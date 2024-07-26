f629e8149a05cb3cf9030fc9b141ce43ec4eec0a
Issam Benelgada <issambenelgada@gmail.com>
2024-07-26 12:37:49 +0200

    Added persistent askedForNotificationPermissionBefore value.

--- src/Context/Contexts/MainContext/MainContext.tsx
+++ src/Context/Contexts/MainContext/MainContext.tsx
@@ -11,11 +11,13 @@ import { MainContextEffect } from './MainContextEffect';
 export type MainContextDataType = {
   isNewUserState: StatePersistentType<boolean>;
   isUsingLocalAccountState: StatePersistentType<boolean>;
+  askedForNotificationPermissionBeforeState: StatePersistentType<boolean>;
 };
 
 const initialState: MainContextDataType = {
   isNewUserState: StatePersistentDefaultValue(true),
   isUsingLocalAccountState: StatePersistentDefaultValue(false),
+  askedForNotificationPermissionBeforeState: StatePersistentDefaultValue(false),
 };
 
 const MainContext = createContext<MainContextDataType>(initialState);
@@ -27,9 +29,18 @@ type PropsType = {
 export const MainContextProvider: React.FC<PropsType> = props => {
   const isNewUserState = useStatePersistent(true, 'IS_NEW_USER');
   const isUsingLocalAccountState = useStatePersistent(false, 'IS_USING_LOCAL_ACCOUNT');
+  const askedForNotificationPermissionBeforeState = useStatePersistent(
+    false,
+    'ASKED_FOR_NOTIFICATION_PERMISSION_BEFORE',
+  );
 
   return (
-    <MainContext.Provider value={{ isNewUserState, isUsingLocalAccountState }}>
+    <MainContext.Provider
+      value={{
+        isNewUserState,
+        isUsingLocalAccountState,
+        askedForNotificationPermissionBeforeState,
+      }}>
       <MainContextEffect>{props.children}</MainContextEffect>
     </MainContext.Provider>
   );
--- src/Context/Contexts/MainContext/useMainContext.ts
+++ src/Context/Contexts/MainContext/useMainContext.ts
@@ -3,28 +3,45 @@ import { useCallback } from 'react';
 import { useMainContextInner } from './MainContext';
 
 export function useMainContextFunctions() {
-  const { isNewUserState, isUsingLocalAccountState } = useMainContextInner();
+  const {
+    isNewUserState,
+    isUsingLocalAccountState,
+    askedForNotificationPermissionBeforeState,
+  } = useMainContextInner();
 
   const [, , setIsNewUser, clearIsNewUser] = isNewUserState;
   const [, , setIsUsingLocalAccount, clearIsUsingLocalAccount] = isUsingLocalAccountState;
+  const [
+    ,
+    ,
+    setAskedForNotificationPermissionBefore,
+    clearAskedForNotificationPermissionBefore,
+  ] = askedForNotificationPermissionBeforeState;
 
   const clearContext = useCallback(() => {
     clearIsNewUser();
     clearIsUsingLocalAccount();
-  }, [clearIsNewUser, clearIsUsingLocalAccount]);
+    clearAskedForNotificationPermissionBefore();
+  }, [clearIsNewUser, clearIsUsingLocalAccount, clearAskedForNotificationPermissionBefore]);
 
   return {
     setIsNewUser,
     setIsUsingLocalAccount,
+    setAskedForNotificationPermissionBefore,
     clearContext,
   };
 }
 
 export function useMainContext() {
-  const { isNewUserState, isUsingLocalAccountState } = useMainContextInner();
+  const {
+    isNewUserState,
+    isUsingLocalAccountState,
+    askedForNotificationPermissionBeforeState,
+  } = useMainContextInner();
 
   const [isNewUser, isNewUserLoaded] = isNewUserState;
   const [isUsingLocalAccount, isUsingLocalAccountLoaded] = isUsingLocalAccountState;
+  const [askedForNotificationPermissionBefore] = askedForNotificationPermissionBeforeState;
 
   const isContextLoaded = isNewUserLoaded && isUsingLocalAccountLoaded;
 
@@ -34,5 +51,6 @@ export function useMainContext() {
     isUsingLocalAccount,
     isUsingLocalAccountLoaded,
     isContextLoaded,
+    askedForNotificationPermissionBefore,
   };
 }
