af336f17fc02da16d807c0c4ca1921aec738e0eb
Issam Benelgada <issambenelgada@gmail.com>
2024-07-27 03:04:13 +0200

    Removed forger password from login screen.

--- src/Components/LoginComponents/LoginForm.tsx
+++ src/Components/LoginComponents/LoginForm.tsx
@@ -76,7 +76,6 @@ export default function LoginForm() {
                 value={values.password}
                 error={errors.password}
               />
-              <ForgotPassword />
             </>
           </ViewWithGap>
           <PrimaryButtonExtraWide
