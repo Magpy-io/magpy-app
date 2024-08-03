c9675076a3cf0877196414086719adfbf945baea
Issam Benelgada <issambenelgada@gmail.com>
2024-08-03 03:07:25 +0200

    Added react-native-udp and new discovery service.

--- src/Context/Contexts/LocalServersContext/LocalServersEffects.tsx
+++ src/Context/Contexts/LocalServersContext/LocalServersEffects.tsx
@@ -1,38 +1,9 @@
-import React, { ReactNode, useEffect } from 'react';
-
-import { MdnsServiceEvents } from '~/NativeModules/MdnsServiceModule';
-
-import { useLocalServersContextSetters } from './LocalServersContext';
-import { useLocalServersFunctions } from './useLocalServersContext';
+import React, { ReactNode } from 'react';
 
 type PropsType = {
   children: ReactNode;
 };
 
 export const LocalServersEffects: React.FC<PropsType> = props => {
-  const { setIsScanning } = useLocalServersContextSetters();
-
-  const { addService } = useLocalServersFunctions();
-
-  useEffect(() => {
-    const subStart = MdnsServiceEvents.subscribeOnSearchStarted(() => {
-      setIsScanning(true);
-    });
-
-    const subStop = MdnsServiceEvents.subscribeOnSearchStopped(() => {
-      setIsScanning(false);
-    });
-
-    const subResolved = MdnsServiceEvents.subscribeOnServiceResolved(service => {
-      addService(service);
-    });
-
-    return () => {
-      subStart.remove();
-      subStop.remove();
-      subResolved.remove();
-    };
-  }, [addService, setIsScanning]);
-
   return props.children;
 };
---
+++ src/Helpers/ServerDiscovery.ts
@@ -0,0 +1,177 @@
+import { EventArg, EventEmitter } from '@react-navigation/native';
+import dgram from 'react-native-udp';
+import UdpSocket from 'react-native-udp/lib/types/UdpSocket';
+import { isIP, isPort } from 'validator';
+
+import { serverDiscoveryPort } from '~/Config/config';
+
+import { sleep } from './Utilities';
+
+export type DiscoveryResponse = {
+  domain: 'magpy-discovery';
+  type: 'response';
+  name: string;
+  ip: string;
+  port: string;
+};
+
+export type DiscoveryRequest = {
+  domain: 'magpy-discovery';
+  type: 'request';
+};
+
+export type Server = {
+  name: string;
+  ip: string;
+  port: string;
+};
+
+const PORT_MIN = 49152;
+const PORT_MAX = 65535;
+
+export class ServerDiscovery {
+  socket: UdpSocket | null;
+  state: 'starting' | 'listening' | 'stopped';
+
+  constructor() {
+    this.socket = null;
+    this.state = 'stopped';
+  }
+
+  async launchDiscovery(onServerDiscovered: (response: Server) => void) {
+    console.log('Discovery started.....................');
+
+    if (this.state != 'stopped') {
+      console.log('Discovery already running.....................');
+      throw new Error('Cannot start new discovery while a discovery is still running.');
+    }
+
+    this.state = 'starting';
+
+    try {
+      this.socket = dgram.createSocket({ type: 'udp4' });
+
+      this.socket.on('error', e => console.log('ServerDiscovery error:', e));
+
+      this.setOnServerDiscovered(onServerDiscovered);
+
+      await this.bindSocket();
+
+      console.log('Socket binded, listening started............................');
+      if (!this.socket) {
+        console.log('Listening aborted............................');
+        return;
+      }
+
+      const address = this.socket.address();
+      console.log('UDP socket listening on ' + address.address + ':' + address.port);
+
+      // Should call setBroadcast on socket but I'm not doing it because I'm using a forked version
+      // of the react-native-udp package and changed it to setBroadcast by default on socket bind
+      // This change was made because of a bug in the package regarding setBroadcast
+      // https://github.com/tradle/react-native-udp/issues/82
+
+      //this.socket.setBroadcast(true);
+
+      await this.sendMessage();
+    } catch (e) {
+      this.socket?.close();
+      this.socket = null;
+      this.state = 'stopped';
+    }
+
+    this.state = 'listening';
+  }
+
+  stop() {
+    console.log('Stop Called.........................');
+    if (this.state == 'starting') {
+      console.log('Discovery is stating up, cannot stop yet.....................');
+      throw new Error('Cannot stop discovery while starting up discovery.');
+    }
+
+    if (this.state == 'stopped') {
+      return;
+    }
+
+    this.socket?.close();
+    this.socket = null;
+    this.state = 'stopped';
+  }
+
+  private async sendMessage() {
+    const request: DiscoveryRequest = { domain: 'magpy-discovery', type: 'request' };
+    const message = JSON.stringify(request);
+
+    return new Promise(res => {
+      this.socket?.send(
+        message,
+        0,
+        message.length,
+        serverDiscoveryPort,
+        '255.255.255.255',
+        () => {
+          res(null);
+        },
+      );
+    });
+  }
+
+  private async bindSocket() {
+    return new Promise(res => {
+      const randomPort = Math.floor(Math.random() * (PORT_MAX - PORT_MIN) + PORT_MIN);
+      this.socket?.bind(randomPort, () => {
+        res(null);
+      });
+    });
+  }
+
+  private setOnServerDiscovered(fun: (response: Server) => void) {
+    if (this.socket != null) {
+      this.socket.removeAllListeners('message');
+      this.socket.on(
+        'message',
+        (message: Buffer, remote: { address: string; port: string }) => {
+          console.log(
+            'Discovery response received from :',
+            remote.address,
+            '. Message: ',
+            message.toString(),
+          );
+
+          let response: DiscoveryResponse;
+          try {
+            response = JSON.parse(message.toString()) as DiscoveryResponse;
+          } catch (e) {
+            console.log('Invalid discovery response.');
+            return;
+          }
+
+          if (response.domain != 'magpy-discovery' || response.type != 'response') {
+            console.log('Invalid discovery response.');
+            return;
+          }
+
+          if (
+            !isIP(response.ip) ||
+            !isPort(response.port) ||
+            !(typeof response.name === 'string')
+          ) {
+            console.log('Invalid discovery response.');
+            return;
+          }
+
+          const remoteIp = remote.address;
+
+          if (remoteIp != response.ip) {
+            console.log(
+              'Discovery message, response IP is different than response socket ip.',
+            );
+          }
+
+          fun({ name: response.name, ip: remoteIp, port: response.port });
+        },
+      );
+    }
+  }
+}
