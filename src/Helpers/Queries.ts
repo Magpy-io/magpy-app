f471a32895c89625788a6af3377548ded8ca2b52
Issam Benelgada <issambenelgada@gmail.com>
2024-10-13 14:04:43 +0200

    Removed unused function.

--- src/Helpers/Queries.ts
+++ src/Helpers/Queries.ts
@@ -1,7 +1,5 @@
-import { uniqueDeviceId } from '~/Config/config';
-import { AddPhotoInit, AddPhotoPart, GetPhotoPartById } from '~/Helpers/ServerQueries';
+import { GetPhotoPartById } from '~/Helpers/ServerQueries';
 
-import { RangeSplitterLinear } from './RangeSplitter';
 import { GetPhotos } from './ServerQueries';
 import { APIPhoto } from './ServerQueries/Types';
 
@@ -34,72 +32,6 @@ export async function getPhotoWithProgress(
   }
   response.data.photo.image64 = image64Parts.join('');
   return response;
-}
-
-export async function addPhotoWithProgress(
-  photo: {
-    date: string;
-    fileSize: number;
-    height: number;
-    name: string;
-    mediaId: string;
-    width: number;
-    image64: string;
-  },
-  f?: (progess: number, total: number) => void,
-): Promise<APIPhoto> {
-  const base64Image = photo.image64;
-
-  const base64ImageSplit = splitString(base64Image);
-
-  const response = await AddPhotoInit.Post({
-    name: photo.name,
-    fileSize: photo.fileSize,
-    width: photo.width,
-    height: photo.height,
-    date: photo.date,
-    mediaId: photo.mediaId,
-    image64Len: base64Image.length,
-    deviceUniqueId: uniqueDeviceId,
-  });
-
-  if (!response.ok) {
-    throw new Error(response.errorCode);
-  }
-
-  if (response.data.photoExistsBefore) {
-    return response.data.photo;
-  }
-
-  const id = response.data.id;
-
-  let responseI;
-  for (let i = 0; i < base64ImageSplit.length; i++) {
-    responseI = await AddPhotoPart.Post({
-      id: id,
-      partNumber: i,
-      partSize: base64ImageSplit[i].length,
-      photoPart: base64ImageSplit[i],
-    });
-
-    if (!responseI.ok) {
-      throw new Error(responseI.errorCode);
-    }
-
-    f?.(i, base64ImageSplit.length);
-  }
-
-  if (!responseI?.data.done) {
-    throw new Error('Sent all parts but photo transfer not done.');
-  }
-
-  return responseI.data.photo;
-}
-
-function splitString(str: string) {
-  return new RangeSplitterLinear(100000)
-    .splitRange(str.length)
-    .map(range => str.substring(range.start, range.end));
 }
 
 export async function getPhotosBatched({
