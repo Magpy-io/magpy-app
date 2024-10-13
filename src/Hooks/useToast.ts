4700619ff4f7272f4233d9677a5f714789a4faaf
Issam Benelgada <issambenelgada@gmail.com>
2024-10-13 18:34:52 +0200

    Added ignore all showToast requests while a toast is displayed.

--- src/Hooks/useToast.ts
+++ src/Hooks/useToast.ts
@@ -2,12 +2,25 @@ import { useCallback } from 'react';
 
 import Toast from 'react-native-toast-message';
 
+let lastToastTime = 0;
+
+const TOAST_VISIBILITY_TIME = 3000;
+
 export function useToast() {
   const showToastError = useCallback((text: string) => {
+    const timeNow = Date.now();
+
+    if (timeNow - lastToastTime < TOAST_VISIBILITY_TIME) {
+      return;
+    }
+
+    lastToastTime = timeNow;
+
     Toast.show({
       type: 'error',
       text1: text,
       position: 'top',
+      visibilityTime: TOAST_VISIBILITY_TIME,
     });
   }, []);
 
