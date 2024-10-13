cf6473fbff6236dd377d61a3e248317c4e75a378
Issam Benelgada <issambenelgada@gmail.com>
2024-10-13 12:39:47 +0200

    Fixed navigation bar turns white when a modal is displayed in dark mode.

--- src/Context/Contexts/ThemeContext/ThemeEffect.tsx
+++ src/Context/Contexts/ThemeContext/ThemeEffect.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode, useEffect } from 'react';
 import { StatusBar } from 'react-native';
+import { Appearance } from 'react-native';
 
 import { DarkTheme, LightTheme } from '~/Styles/colors';
 
@@ -16,6 +17,7 @@ export const ThemeEffect: React.FC<PropsType> = props => {
     const theme = dark ? DarkTheme : LightTheme;
     StatusBar.setBarStyle(dark ? 'light-content' : 'dark-content');
     StatusBar.setBackgroundColor(theme.colors.BACKGROUND);
+    Appearance.setColorScheme(dark ? 'dark' : 'light');
   }, [dark]);
 
   return props.children;
