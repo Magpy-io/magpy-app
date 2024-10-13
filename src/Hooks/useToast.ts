a6fce0543be32fdc6cebf23475285bcef476f489
Issam Benelgada <issambenelgada@gmail.com>
2024-10-13 16:22:23 +0200

    Fixed bug causing re-renders.

--- src/Hooks/useToast.ts
+++ src/Hooks/useToast.ts
@@ -1,21 +1,19 @@
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useCallback } from 'react';
+
 import Toast from 'react-native-toast-message';
 
 export function useToast() {
-  const insets = useSafeAreaInsets();
-
-  const showToastError = (text: string, position?: 'bottom' | 'top') => {
+  const showToastError = useCallback((text: string) => {
     Toast.show({
       type: 'error',
       text1: text,
-      position: position ?? 'top',
-      bottomOffset: insets.bottom + 5,
+      position: 'top',
     });
-  };
+  }, []);
 
-  const hideToast = () => {
+  const hideToast = useCallback(() => {
     Toast.hide();
-  };
+  }, []);
 
   return { showToastError, hideToast };
 }
