24657967dc1ff93da7521da23382852edb19801e
Issam Benelgada <issambenelgada@gmail.com>
2024-10-15 20:40:28 +0200

    Added uuide library.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { AppRegistry } from 'react-native';
 
+// This library works as a polyfill for the global crypto.getRandomValues.
+// This will allow the uuid library to work
+import 'react-native-get-random-values'
+
 import App from './App';
 import { name } from './app.json';
 
