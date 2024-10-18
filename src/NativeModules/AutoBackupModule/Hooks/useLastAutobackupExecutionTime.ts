694f6b000349bd4fb3b293a10917baf62b7af300
Issam Benelgada <issambenelgada@gmail.com>
2024-10-18 22:29:50 +0200

    Fixed bug in useLastAutobackupExecutionTime hook.

--- src/NativeModules/AutoBackupModule/Hooks/useLastAutobackupExecutionTime.ts
+++ src/NativeModules/AutoBackupModule/Hooks/useLastAutobackupExecutionTime.ts
@@ -6,7 +6,7 @@ export function useLastAutobackupExecutionTime() {
   const [time, setTime] = useState<Date | null>(null);
 
   useEffect(() => {
-    const handle = setTimeout(() => {
+    const handle = setInterval(() => {
       AutoBackupModule.GetWorkerStats()
         .then(workerStats => {
           if (!workerStats.lastExecutionTime) {
--- src/NativeModules/AutoBackupModule/index.ts
+++ src/NativeModules/AutoBackupModule/index.ts
@@ -1,1 +1,2 @@
 export * from './AutoBackupModule';
+export * from './Hooks';
