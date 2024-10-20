c3061a281429b4922bd6dbe7af9aa5b667d229fc
Issam Benelgada <issambenelgada@gmail.com>
2024-10-20 03:23:46 +0200

    Added default port in manual server search connect page.

--- src/Config/config.ts
+++ src/Config/config.ts
@@ -3,3 +3,4 @@ export const uniqueDeviceId = '7a83660e-9509-4e94-9dc8-7c5ef3dbef83';
 export const websiteUrl = 'https://magpy.io';
 export const privacyPolicyUrl = 'https://magpy.io/privacy';
 export const serverDiscoveryPort = 38951;
+export const defaultServerPort = '31803';
