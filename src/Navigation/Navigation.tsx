15aed2e5a5498c6fb39cca479faa289be7b52adc
Issam Benelgada <issambenelgada@gmail.com>
2024-10-20 14:25:30 +0200

    Fixed a theme bug.

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { useColorScheme } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { DarkTheme, DefaultTheme } from '@react-navigation/native';
 
+import { useTheme } from '~/Context/Contexts/ThemeContext';
 import { TabNavigationProvider } from '~/Navigation/TabNavigation/TabNavigationContext';
 
 import { Root } from './Root';
 
 const Navigation = () => {
-  const scheme = useColorScheme();
+  const { dark } = useTheme();
 
   return (
-    <NavigationContainer theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <NavigationContainer theme={dark ? DarkTheme : DefaultTheme}>
       <TabNavigationProvider>
         <Root />
       </TabNavigationProvider>
