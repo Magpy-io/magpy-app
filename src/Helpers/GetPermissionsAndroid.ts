47b8542feb249a2c8a852acff3131b9516f97a23
Issam Benelgada <issambenelgada@gmail.com>
2024-10-23 18:27:48 +0200

    Fixed permissions not requested on API 31 (Android 12).

--- src/Helpers/GetPermissionsAndroid.ts
+++ src/Helpers/GetPermissionsAndroid.ts
@@ -1,36 +1,68 @@
 import { Permission, PermissionsAndroid, Platform } from 'react-native';
 
 export async function hasAndroidPermissionReadMedia() {
-  const imagesPermission = await hasAndroidAnyPermission(
-    PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
-  );
-  const videosPermission = await hasAndroidAnyPermission(
-    PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
-  );
+  if (Platform.OS !== 'android') {
+    throw new Error('Only use hasAndroidPermission on Android, Platform.OS == ' + Platform.OS);
+  }
 
-  return imagesPermission && videosPermission;
+  if (Platform.Version > 31) {
+    const imagesPermission = await hasAndroidAnyPermission(
+      PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+    );
+    const videosPermission = await hasAndroidAnyPermission(
+      PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+    );
+
+    return imagesPermission && videosPermission;
+  } else {
+    const permission = await hasAndroidAnyPermission(
+      PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+    );
+
+    return permission;
+  }
 }
 
 export async function askAndroidPermissionReadMedia() {
-  const result = await PermissionsAndroid.requestMultiple([
-    PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
-    PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
-  ]);
+  if (Platform.OS !== 'android') {
+    throw new Error('Only use hasAndroidPermission on Android, Platform.OS == ' + Platform.OS);
+  }
 
-  const permissionsGranted =
-    result[PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES] ==
-      PermissionsAndroid.RESULTS.GRANTED &&
-    result[PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO] ==
-      PermissionsAndroid.RESULTS.GRANTED;
+  if (Platform.Version > 31) {
+    const result = await PermissionsAndroid.requestMultiple([
+      PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+      PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+    ]);
 
-  return permissionsGranted;
+    const permissionsGranted =
+      result[PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES] ==
+        PermissionsAndroid.RESULTS.GRANTED &&
+      result[PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO] ==
+        PermissionsAndroid.RESULTS.GRANTED;
+
+    return permissionsGranted;
+  } else {
+    const result = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+    );
+
+    return result == PermissionsAndroid.RESULTS.GRANTED;
+  }
 }
 
 export async function hasAndroidPermissionNotifications() {
+  if (Platform.OS !== 'android') {
+    throw new Error('Only use hasAndroidPermission on Android, Platform.OS == ' + Platform.OS);
+  }
+
   return await hasAndroidAnyPermission(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
 }
 
 export async function askAndroidPermissionNotifications() {
+  if (Platform.OS !== 'android') {
+    throw new Error('Only use hasAndroidPermission on Android, Platform.OS == ' + Platform.OS);
+  }
+
   const result = await PermissionsAndroid.request(
     PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
   );
