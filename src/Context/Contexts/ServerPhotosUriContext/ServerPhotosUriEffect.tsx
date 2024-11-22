b87ce79ab44deb33a4949248f49fbc4881956ef0
Issam Benelgada <issambenelgada@gmail.com>
2024-11-22 23:43:36 +0100

    Removed unused context.

--- src/Context/Contexts/ServerPhotosUriContext/ServerPhotosUriEffect.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react';
-
-type PropsType = {
-  children: ReactNode;
-};
-
-export const ServerPhotosUriEffect: React.FC<PropsType> = props => {
-  return props.children;
-};
